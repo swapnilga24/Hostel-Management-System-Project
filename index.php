f65db17fb24f62fe91d3f0a9fdfefe487e9edc86
swapnilga24 <39656416+swapnilga24@users.noreply.github.com>
2019-10-01 11:31:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE>
+<html>
+    <head>
+        <title>VIIT HOSTEL</title>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+        
+       <link rel="stylesheet" type="text/css" href="stylesheets/home.css">
+    </head>
+    <body>
+        <iframe width="0" height="0" src="bensound-anewbeginning.mp3" frameborder="0" allowfullscreen></iframe>
+        <nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="home.php" class="navbar-brand">VIIT Hostel</a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#hamburger" aria-expanded="false">  <!--hamburger button-->
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>          <!--lines on that icon-->
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="hamburger">
+                    <ul class="nav navbar-nav">
+                         <li class="active"><a href="index.php">Home</a></li>   <!--to make tab active-->
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                         <li><a href="login.php">New Registration <i class="fas fa-user"></i></a></li>
+                    </ul>
+                </div> 
+            </div>
+        </nav>
+        <div class="container">
+    
+            </br></br>
+            <h1 style=" margin : 4%;width: 900;height: 60;text-align: center;background-color: lightgreen;">VIIT HOSTEL MANAGEMENT SYSTEM</h1>
+            <hr>
+             <div style="float : right;width : 38%; margin : 0 130px 0 0  ; ">
+                 <h2 style="font-family: 'Noto Serif', serif; 
+                color : #2635FF;
+                font-weight: 800;text-align: center;">Map Location</h2>
+                <div style="float : right;width : 38%; margin : 0 130px 0 0;">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.5166505695784!2d73.88078101526838!3d18.460242487444162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2ea7d13bfba81%3A0x29d2e67fbfc36f75!2sVishwakarma+Institute+Of+Information+Technology!5e0!3m2!1sen!2sin!4v1496754021897"></iframe>
+                </div>
+            </div>
+            <div class="container" style="float:left;width :50%">
+                <h1 style="font-family: 'Noto Serif', serif; 
+                color : #2635FF;
+                font-weight: 800;">Rules And Regulations</h1>
+
+                <p>
+                    Ragging is strictly prohibited. The Maharashtra Legislative Council has passed a bill (L C Bill Number ix of 1999) <strong>to prohibit ragging</strong> in educational institutions in the State of Maharashtra on 7th April 1999. <br>As per this bill :-
+                    <strong>Ragging within or outside of any educational institution is strictly prohibited.</strong><br />
+                    Whosoever directly or indirectly commits, participates in, abets or progagates ragging within or outside any educational institution shall on conviction, be punished, as per the rules. <br />
+                    Any student convicted of an offence of ragging shall be dismissed from the educational institution and such student shall not be admitted 	to any other educational institution for a period of <strong>five years</strong> from the date of order of such dismissal.
+                </p>
+        </br>
+                <marquee behavior="alternate" direction="up" width="80%">
+                    <marquee direction="right" behavior="alternate">
+                <div class="section field field-name-field-download-as-file field-type-file field-label-hidden"><div class="field-items"><span class="file"><a href="http://www.viit.ac.in/images/Admissions/HOSTEL-FEE-STRUCTURE_2018-19.pdf"; length=94240">College information._2018-19.pdf</a></span></div></div>
+                </marquee>
+                </marquee>
+            </div>
+        </div>
+    </body>
+</html>
